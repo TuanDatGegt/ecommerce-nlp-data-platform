cfbc371073cd062e726737c7d27a853dac6e5a1f
TuanDatGegt <trantuandat23012003@gmail.com>
2026-05-13 22:33:45 +0700

    change structure for stage 0

--- scripts/run_bronze_ingestion.py
+++ scripts/run_bronze_ingestion.py
@@ -1,40 +1,92 @@
-import glob, os, yaml
+#scritps/run_bronze_ingestion.py
+
+import glob
+import os
+
 from concurrent.futures import ProcessPoolExecutor
-from prerocessing import process_tsv_file, data_extract_category
-from config import RAW_DATA_PATH, PROCESSED_DATA_PATH, yaml_config_path, MAX_WORKERS
+from tqdm import tqdm
 
-def run_batch_processing(input_path):
-    """
-    This functions has been run single file, before I run it for all files in the directory, 
-    I want to make sure that the processing works correctly for one file.
-    """
-    fileName = os.path.basename(input_path)
-    category = data_extract_category(fileName)
+from configs.settings import (
+    RAW_DATA_PATH,
+    PROCESSED_DATA_PATH,
+    MAX_WORKERS
+)
 
-    process_tsv_file(
-        input_file=input_path,
-        output_base=PROCESSED_DATA_PATH
+from pipeline.bronze.ingrest import read_tsv_in_chunks
+
+from pipeline.bronze.transform import (
+    optimize_dataframe,
+    add_metadata_columns
+)
+
+from pipeline.bronze.validate import (
+    validate_required_columns,
+    write_to_dlq
+)
+
+from pipeline.bronze.writer import (
+    write_parquet_chunk
+)
+
+from pipeline.bronze.metadata import (
+    extract_category_from_filename,
+    build_partition_path
+)
+
+from utils.logger import setup_logger
+
+logger = setup_logger(
+    "bronze_ingestion",
+    "logs/bronze_ingestion.log")
+
+def process_single_file(input_file):
+    file_name = os.path.basename(input_file)
+    category = extract_category_from_filename(file_name)
+
+    output_dir = build_partition_path(
+        PROCESSED_DATA_PATH,
+        category
     )
 
+    try: 
+        reader = read_tsv_in_chunks(input_file)
+        for i, chunk in enumerate(reader):
+            chunk = optimize_dataframe(chunk)
+            chunk = add_metadata_columns(
+                chunk, 
+                category
+            )
+
+        validate_required_columns(chunk)
+        path = write_parquet_chunk(
+            chunk,
+            output_dir,
+            i
+        )
+        logger.info(
+            f"Processed {path}"
+        )
+
+    except Exception as e:
+        logger.error(
+            f"FAILED {file_name} | {str(e)}"
+        )
+
+        write_to_dlq(chunk, file_name)
+    
     return category
 
-def save_categories_yaml(categories, output_yaml):
-    """
-    Save the list of categories to a YAML file.
-    """
-    data = {
-        "categories": sorted(list(set(categories)))
-    }
-    with open(output_yaml, 'w') as f:
-        yaml.dump(data, f, default_flow_style=False)
-
-
 if __name__ == "__main__":
-    input_files = glob.glob(os.path.join(RAW_DATA_PATH, "*.tsv"))
-    categories = []
 
-    with ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        for category in executor.map(run_batch_processing, input_files):
-            categories.append(category)
+    input_files = glob.glob(
+        os.path.join(RAW_DATA_PATH, "*.tsv")
+    )
 
-    save_categories_yaml(categories, yaml_config_path)
+    with ProcessPoolExecutor(
+        max_workers=MAX_WORKERS
+    ) as executor:
+        list(tqdm(
+            executor.map(process_single_file, input_files),
+            total=len(input_files)
+        ))
+
