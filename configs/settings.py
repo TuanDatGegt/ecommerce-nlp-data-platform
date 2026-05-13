58d654592b230e97f063d9a1d2a314561efe1576
TuanDatGegt <trantuandat23012003@gmail.com>
2026-05-13 22:51:44 +0700

    change dir sample dataset and data lake

--- configs/settings.py
+++ configs/settings.py
@@ -1,10 +1,13 @@
+#config/settings.py
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 RAW_DATA_PATH = os.getenv('RAW_DATA_PATH')
-PROCESSED_DATA_PATH = os.getenv('PROCESSED_DATA_PATH')
+DATA_LAKE_PATH = os.getenv('DATA_LAKE_PATH')
+SAMPLE_DATA_PATH = os.getenv('SAMPLE_DATA_PATH')
+
 yaml_config_path = os.getenv('YAML_CONFIG_PATH')
 
 MAX_WORKERS = int(os.getenv('MAX_WORKERS', 4))
@@ -13,4 +16,3 @@ CHUNK_SIZE = 200_000
 COMPRESSION = 'snappy'
 
 CATEGORY_MAPPING = r"amazon_reviews_us_([A-Za-z0-9_\-]+)_v"
-    
