f2367d8ca51664e996b40a67386d9c3faa034727
TuanDatGegt <trantuandat23012003@gmail.com>
2026-05-14 02:03:40 +0700

    add needed columns for pineline

--- configs/schemas/review_schemas.py
+++ configs/schemas/review_schemas.py
@@ -2,8 +2,12 @@
 
 REQUIRED_COLUMNS = [
     'review_id',
+    'customer_id',      # Thêm để định danh khách hàng
     'product_id',
-    'reviewer_body',
+    'product_category',  # Thêm để phân loại ngành hàng
+    'review_body',
+    'review_headline',   # Thêm để lấy tiêu đề review
     'star_rating',
+    'verified_purchase', # Thêm để kiểm tra độ xác thực
     'review_date',
 ]
