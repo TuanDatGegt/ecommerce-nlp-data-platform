cc8f9c685c54a00ddcff902b14125c1b45b9e04a
TuanDatGegt <trantuandat23012003@gmail.com>
2026-05-29 07:11:07 +0700

    get checking validate from authories API and download dataset from kaggle

---
+++ pipeline/bronze/kaggle/downloader.py
@@ -0,0 +1,25 @@
+#pipeline/bronze/kaggle/downloader.py
+
+import os
+import kagglehub
+
+from configs.settings import KAGGLE_DATASET
+
+from pipeline.bronze.kaggle.auth import validate_kaggle_credentials
+
+def download_kaggle_dataset():
+
+    validate_kaggle_credentials()
+
+    print(
+        f"Downloading dataset {KAGGLE_DATASET}"
+    )
+
+    dataset_path = kagglehub.dataset_download(KAGGLE_DATASET, force=True)
+
+    print(
+        f"Dataset downloaded to {dataset_path}"
+    )
+
+    return dataset_path
+
