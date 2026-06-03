1f42281579b2d13d74b70146568b4c62c0ff02f8
TuanDatGegt <trantuandat23012003@gmail.com>
2026-06-03 13:56:19 +0700

    add auto_init=False

--- pipeline/bronze/dlq/dead_letter_queue.py
+++ pipeline/bronze/dlq/dead_letter_queue.py
@@ -9,7 +9,7 @@ from pipeline.bronze.storage.minio_client import MinioClient
 
 from configs.settings import RAW_BUCKET_NAME
 
-storage = MinioClient()
+storage = MinioClient(auto_init=False)
 
 def write_to_dlq(df, source_file, error_message):
     temp_dir = tempfile.mkdtemp()
@@ -18,21 +18,28 @@ def write_to_dlq(df, source_file, error_message):
     file_name =(f"{source_file}_{timestamp}.json")
 
     local_file = os.path.join(temp_dir, file_name)
-    records = df.to_dict(orient='records')
+
+    if df is None and hasattr(df, "to_dict"):
+        records = df.to_dict(orient='records')
+    else: 
+        records = [{"raw_data_error": str(df)}]
 
     payload={
         "source_file":  source_file,
-        "error_message": error_message,
+        "error_message": str(error_message),
         "failed_at": str(datetime.now(timezone.utc)),
-        records: records
+        "records": records
     }
 
     with open(local_file, "w")as f:
         json.dump(payload, f, indent=4, default=str)
 
-    object_name = os.path.join('dlq', "bronze", file_name)
+    object_name = f"dlq/bronze/{file_name}"
 
-    storage.upload_file(bucket_name=RAW_BUCKET_NAME, file_path=local_file, object_name=object_name)
+    storage.upload_file(
+        bucket_name=RAW_BUCKET_NAME,
+        file_path=local_file,
+        object_name=object_name)
     
     os.remove(local_file)
     return object_name
