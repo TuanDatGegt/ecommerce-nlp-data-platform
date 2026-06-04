d50f26123dc2c5409908e1e5723114ca5358ea0e
TuanDatGegt <trantuandat23012003@gmail.com>
2026-06-04 21:39:24 +0700

    fix the tempdir path

--- pipeline/bronze/processing/writer.py
+++ pipeline/bronze/processing/writer.py
@@ -3,14 +3,15 @@ import os
 import tempfile
 
 from pipeline.bronze.storage.minio_client import MinioClient
-#from configs.settings import MINIO_BUCKET_NAME
+from configs.settings import TEMPDIR_PATH
 
 storage = MinioClient()
 
 
 def write_parquet_chunk(df, output_prefix, chunk_idx):
+    os.makedirs(TEMPDIR_PATH, exist_ok=True)
 
-    temp_dir = tempfile.mkdtemp()
+    temp_dir = tempfile.mkdtemp(dir=TEMPDIR_PATH, prefix=f"{output_prefix}_tmp_")
     local_file = os.path.join(temp_dir, f"part_{chunk_idx:05d}.parquet") 
     df.to_parquet(local_file, index=False, compression='snappy')
 
